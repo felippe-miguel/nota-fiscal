caf54e30f49ec77688637c3604d88c3905422cde
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-09-18 02:07:47 -0300

    Criando as views

---
+++ app/resources/views/lojas/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Lojas')
+
+@section('content_header')
+    <h1>Lojas</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
