5a684a955a67c18cb1f930faffaa6ef96dd15c43
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-09-18 02:08:03 -0300

    Configurando as rotas dos resources

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\SessaoController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\LojaController;
+use App\Http\Controllers\NotaController;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('categorias', CategoriaController::class);
+Route::resource('sessoes', SessaoController::class);
+Route::resource('produtos', ProdutoController::class);
+Route::resource('lojas', LojaController::class);
+Route::resource('notas', NotaController::class);
